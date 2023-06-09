c12d92e247494445bd58ed54f99ba4e6a20c562b
Rahatul07 <rifarhad08@gmail.com>
2023-06-09 11:48:29 +0600

    design improved

--- src/pages/Home/InstructorCard/InstructorCard.jsx
+++ src/pages/Home/InstructorCard/InstructorCard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 const InstructorCard = ({ instructor }) => {
-  const { image } = instructor;
+  const { image, instructorName } = instructor;
+  console.log(instructor);
   return (
     <div className="">
       <div className="grid grid-cols-3 w-10/12 mx-auto mb-20 gap-10">
@@ -17,12 +18,12 @@ const InstructorCard = ({ instructor }) => {
               />
             </div>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-              <h1 className="font-dmserif text-2xl text-primary font-bold uppercase mb-20  animate-bounce">
-                Alex
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 -mb-10">
+              <h1 className="font-dmserif text-2xl text-primary font-bold uppercase    animate-bounce">
+                {instructorName}
               </h1>
 
-              <div className="card-actions inline-flex items-center justify-between">
+              <div className="card-actions inline-flex items-center  justify-between">
                 <button className="rounded-none  py-1 mb-3 px-3.5    text-primary text-4xl   flex gap-5">
                   <BsFacebook />
                   <BsLinkedin />
