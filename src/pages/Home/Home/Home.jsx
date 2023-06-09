6b987bd42f4d21805c5a404322f50e8c65bdafbc
Rahatul07 <rifarhad08@gmail.com>
2023-06-09 15:09:27 +0600

    Helmet added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,10 +4,14 @@ import Classes from "../Classes/Classes";
 import Banner from "../Banner/Banner";
 import Features from "../Features/Features";
 import FeaturedInstructors from "../FeaturedInstructors/FeaturedInstructors";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Language-School | Home</title>
+      </Helmet>
       <Banner />
       <Features />
       <Classes />
